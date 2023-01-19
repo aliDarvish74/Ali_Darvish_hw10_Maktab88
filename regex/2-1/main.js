947dab388e78599706bdf835f93887b538c16157
Ali Darvish <ali.darvish.sf@gmail.com>
2023-01-19 10:56:43 +0330

    Solve 2-1 Regex

---
+++ regex/2-1/main.js
@@ -0,0 +1,23 @@
+const nameFormat = /^([a-zA-Z]){1,30}$/;
+const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,})+$/;
+const passwordFormat =
+  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/gm;
+
+const formValidator = () => {
+  $(".form-text").removeClass("text-danger");
+  if (!$("#firstName").val().trim().match(nameFormat)) {
+    $("#firstNameHelp").addClass("text-danger");
+  }
+
+  if (!$("#lastName").val().trim().match(nameFormat)) {
+    $("#lastNameHelp").addClass("text-danger");
+  }
+
+  if (!$("#email").val().trim().match(emailFormat)) {
+    $("#emailHelp").addClass("text-danger");
+  }
+
+  if (!$("#password").val().trim().match(passwordFormat)) {
+    $("#passwordHelp").addClass("text-danger");
+  }
+};
